01c586d52953489bd6e33f2b696035168108b8fb
Kimmo Lepola <kimmolepola@dhcp-asv-91.helsinki.fi>
2015-10-01 09:32:38 +0300

    lisaa

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,9 +17,24 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+
+    if a<=0 or b <=0 or c<=0:
+        raise TriangleError
+
+    k1, k2, h = sorted([a, b, c])
+
+    if k1 + k2 <= h:
+        raise TriangleError
+
+
+    if a==b and b==c:
+        return "equilateral"
+
+    if a==b or b==c or a==c:
+        return "isosceles"
+
+    return "scalene"
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
-    pass
+        pass
